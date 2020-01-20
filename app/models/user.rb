2cf9ccab3248dd0d2f0e18cbb19f7bfe705c8178
ziraqyoung <ziraqyoung@outlook.com>
2020-01-20 12:04:05 +0300

    Fix code with rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,11 @@ class User < ApplicationRecord
   before_save { email.downcase! }
   has_secure_password
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true,
-    length: { maximum: 255 },
-    format: { with: VALID_EMAIL_REGEX },
-    uniqueness: { case_sensitive: false }
-
+                    length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 6 }
 end
