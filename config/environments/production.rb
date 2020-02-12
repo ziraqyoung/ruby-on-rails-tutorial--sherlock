3240c5e7029146649561024419508b6b698cf8c4
ziraqyoung <ziraqyoung@outlook.com>
2020-02-12 15:06:19 +0300

    Add password reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,15 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "sherlock_production"
 
   config.action_mailer.perform_caching = false
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: '587',
+    authentication: 'plain',
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'heroku.com',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
 
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :edit, :create, :update]
 end
